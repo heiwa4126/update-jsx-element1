2c82594128cf95b528f5cf4df5288330a5433cd9
heiwa4126 <heiwa4126@gmail.com>
2023-04-18 09:45:22 +0900

    add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# update-jsx-element1
+
+JSX.Elementの一部を書き換えるテスト。
+[App.tsx](src/App.tsx)の updateImgSrc() を参照。
+
+今回は書き換えだけど、
+応用すれば
+追加や置き換え(imgをnext/imageのImageにする等)が出来る。
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
-import { useState } from "react";
-import "./App.css";
+import React from "react";
+
+const img1 = "/150x150.png";
+const img2 = "/150x150-2.png";
+
+function ImageTest1() {
+  return (
+    <div>
+      <img src={img1} /> <img src={img1} />
+    </div>
+  );
+}
+
+/**
+ * element中の&lt;img&gt;のsrc=を newSrcに置き換える
+ * @param element 置き換え元の JSX.Element
+ * @param newSrc 置き換えるイメージのURL
+ * @returns 更新されたJSX.Element。オリジナルのクローン
+ */
+function updateImgSrc(element: JSX.Element, newSrc: string): JSX.Element {
+  if (element.type === "img") {
+    return React.cloneElement(element, { src: newSrc });
+  }
+
+  const children = React.Children.map(element.props.children, (child) => {
+    if (React.isValidElement(child)) {
+      return updateImgSrc(child, newSrc);
+    }
+    return child;
+  });
+
+  return React.cloneElement(element, {}, children);
+}
 
 function App() {
-  return <div>Hello</div>;
+  const test1 = ImageTest1();
+  const test2 = updateImgSrc(test1, img2);
+  return (
+    <>
+      <div>original</div>
+      {test1}
+      <div>update</div>
+      {test2}
+    </>
+  );
 }
 
 export default App;
